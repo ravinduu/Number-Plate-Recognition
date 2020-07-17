5f6e2d7a36c417fc31c8723815e693a6ecf59652
ravindu <ravindu.dilshan.rd@gmail.com>
2020-07-17 09:39:47 +0530

    extract area

--- NumPlateDetection.py
+++ NumPlateDetection.py
@@ -7,44 +7,41 @@ original_image = cv2.imread('images/car.jpg',1)
 
 # resize original image (width = 500)
 resized_image = imutils.resize(original_image, width=500)
-
 #grayscale
 grayscale_image = cv2.cvtColor(resized_image, cv2.COLOR_BGR2GRAY)
 #denoising
 denoised_image = cv2.medianBlur(grayscale_image, 3)
-
 #edges
 edges = cv2.Canny(denoised_image,100,200)
 
-(cnts, heirarchy) = cv2.findContours(edges.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-cnts=sorted(cnts, key = cv2.contourArea, reverse = True)[:30]
-NumberPlateCnt = None
+#Display the images
+cv2.imshow('Original Image', original_image)
+cv2.imshow('Resized Image', resized_image)
+cv2.imshow('Gray Image', grayscale_image)
+cv2.imshow('Denoised Image', denoised_image)
+cv2.imshow('Edges', edges)
+
+
+contours, hierarchy=cv2.findContours(edges,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+cv2.imshow('canny edges after contouring', edges)
 
 count = 0
-for c in cnts:
+for c in contours:
         peri = cv2.arcLength(c, True)
         approx = cv2.approxPolyDP(c, 0.02 * peri, True)
         if len(approx) == 4:  
             NumberPlateCnt = approx 
             break
 
+# # Drawing the selected contour on the original image
+cv2.drawContours(resized_image, [NumberPlateCnt], -1, (0,255,0), 3)
+cv2.imshow("Final Image With Number Plate Detected", resized_image)
 
-# Drawing the selected contour on the original image
-cv2.drawContours(original_image, [NumberPlateCnt], -1, (0,255,0), 3)
-cv2.imshow("Final Image With Number Plate Detected", original_image)
+# print(contours)
+# print('Numbers of contours found=' + str(len(contours)))
 
-#Display the original image
-cv2.imshow('Original Image', original_image)
-
-#Display the resized image
-# cv2.imshow('Resized Image', resized_image)
-
-#Display the grayscale image
-cv2.imshow('Gray Image', grayscale_image)
-#Display the denoised image
-cv2.imshow('Denoised Image', denoised_image)
-
-cv2.imshow('Edges', edges)
+# cv2.drawContours(resized_image,contours,-1,(0,255,0),3)
+# cv2.imshow('contours',resized_image)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
