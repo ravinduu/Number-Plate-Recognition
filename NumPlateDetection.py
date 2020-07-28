841fa8d66106e5d06fa049725cc6428abf1b0f9a
ravindu <ravindu.dilshan.rd@gmail.com>
2020-07-28 20:05:30 +0530

    edited the code

--- NumPlateDetection.py
+++ NumPlateDetection.py
@@ -2,18 +2,6 @@ import cv2
 import numpy as np 
 import imutils
 
-#read the image
-original_image = cv2.imread('images/car.jpg',1)
-
-# resize original image (width = 500)
-resized_image = imutils.resize(original_image, width=500)
-#grayscale
-grayscale_image = cv2.cvtColor(resized_image, cv2.COLOR_BGR2GRAY)
-#denoising
-denoised_image = cv2.medianBlur(grayscale_image, 3)
-#edges
-edges = cv2.Canny(denoised_image,100,200)
-
 #Display the images
 # cv2.imshow('Original Image', original_image)
 # cv2.imshow('Resized Image', resized_image)
@@ -22,24 +10,39 @@ edges = cv2.Canny(denoised_image,100,200)
 # cv2.imshow('Edges', edges)
 
 
-contours, hierarchy=cv2.findContours(edges,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+def identifyNumberplateArea(edges):
+    contours, hierarchy=cv2.findContours(edges,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
 
-for c in contours:
-        peri = cv2.arcLength(c, True)
-        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
-        if len(approx) == 4:  
-            NumberPlateCnt = approx 
-            break
+    for c in contours:
+            peri = cv2.arcLength(c, True)
+            approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+            if len(approx) == 4:  
+                NumberPlateCnt = approx 
+                break
 
-# # Drawing the selected contour on the original image
-cv2.drawContours(resized_image, [NumberPlateCnt], -1, (0,255,0), 3)
-cv2.imshow("Final Image With Number Plate Detected", resized_image)
+    cv2.drawContours(resized_image, [NumberPlateCnt], -1, (0,255,0), 3)# # Drawing the selected contour on the original image
+    cv2.imshow("Final Image With Number Plate Detected", resized_image)
 
-# print(contours)
-# print('Numbers of contours found=' + str(len(contours)))
+    # print(contours)
+    # print('Numbers of contours found=' + str(len(contours)))
 
-cv2.drawContours(resized_image,contours,-1,(0,255,0),3)
-cv2.imshow('contours',resized_image)
+    cv2.drawContours(resized_image,contours,-1,(0,255,0),3)
+    cv2.imshow('contours',resized_image)
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+def cleanImage():
+    original_image = cv2.imread('images/car.jpg',1)#read the image
+    resized_image = imutils.resize(original_image, width=500) # resize original image (width = 500)
+    grayscale_image = cv2.cvtColor(resized_image, cv2.COLOR_BGR2GRAY) #grayscale
+    denoised_image = cv2.medianBlur(grayscale_image, 3)#denoising
+    edges = cv2.Canny(denoised_image,100,200)#denoising
+
+    identifyNumberplateArea(edges)
+
+
+def main():
+    cleanImage()
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+if __name__ == __main__:
+    main()
